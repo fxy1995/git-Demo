1f9c536405df65f69515fe59886550d8a21861d3
fxy1995 <1460793107@qq.com>
2020-04-10 16:45:59 +0800

    第er次提交代码

--- testGitDemoOne/src/TestMian.java
+++ testGitDemoOne/src/TestMian.java
@@ -1,6 +1,7 @@
 public class TestMian {
     private  int age;
     private String name;
+    private int age2;
     public static void main(String[] args) {
         System.out.println("这是我第一次通过git往github提交代码");
     }

f7af320df4788b39ae70f41374cc20e086af6ed7
fxy1995 <1460793107@qq.com>
2020-04-11 17:41:26 +0800

    test2中新增了一个test2方法

--- testGitTwo/src/test2.java
+++ testGitTwo/src/test2.java
@@ -3,4 +3,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class test2 {
    private int age;
    private String name;
+   public  void test2Method(){
+      System.out.println("我是test2中新增的方法");
+   }
 }

43a5ba3b4b87d429c8cbdebcacbc219f5ae295e6
fxy1995 <1460793107@qq.com>
2020-04-11 15:39:52 +0800

    这是我代码回退测试

--- testGitDemoOne/src/TestMian.java
+++ testGitDemoOne/src/TestMian.java
@@ -2,8 +2,11 @@ public class TestMian {
     private  int age;
     private String name;
     private int age2;
+    private int sex;
     public static void main(String[] args) {
+
         System.out.println("这是我第一次通过git往github提交代码");
+        System.out.println("这是我的回退测试");
     }
 
 }

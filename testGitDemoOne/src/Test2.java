b5375c41bd569906a5ae62bc1ae26228f745e66d
fxy1995 <1460793107@qq.com>
2020-04-11 17:03:12 +0800

    合并分支测试---testbranch

--- testGitDemoOne/src/Test2.java
+++ testGitDemoOne/src/Test2.java
@@ -2,6 +2,6 @@ public class Test2 {
     private  int age;
     private String name;
     public static void main(String[] args) {
-        System.out.println("mark directory is source");
+        System.out.println("我是test banrch ");
     }
 }

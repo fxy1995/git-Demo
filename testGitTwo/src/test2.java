fba760657983f9cf2d1d681b98678b8e9b598d6c
fxy1995 <1460793107@qq.com>
2020-04-11 17:43:12 +0800

    test3 branch 在test2类中新增了一个test3方法

--- testGitTwo/src/test2.java
+++ testGitTwo/src/test2.java
@@ -3,4 +3,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class test2 {
    private int age;
    private String name;
+   public  void  test3(){
+      System.out.println("我是test3在test2中新增的一个方法");
+   }
 }
